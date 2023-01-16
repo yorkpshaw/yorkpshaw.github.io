ba61335f731af20b92a9c7f2d79ba7449beddd40
York Shaw <york.p.shaw@gmail.com>
2023-01-16 13:24:16 -0600

    Resized avatar

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () => {
     <footer className="overflow-hidden rounded-b-2xl bg-slate-50 dark:bg-black">
       <div className="container">
         <p className="text-center py-6 text-gray-lite  dark:text-color-910 ">
-          &copy; {new Date().getFullYear()} All Rights Reserved by{" "}
+          &copy; {new Date().getFullYear()} {" "}
           <a
             className="hover:text-[#FA5252] duration-300 transition"
             href="https://themeforest.net/user/ib-themes"
             target="_blank"
             rel="noopener noreferrer"
           >
-            York Shaw
+            York Shaw. Site powered by Next.JS
           </a>
           .
         </p>
