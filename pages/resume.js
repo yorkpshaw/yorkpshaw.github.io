48c829feb63b8d746dc1c0b9293dec6981510be1
York Shaw <york.p.shaw@gmail.com>
2023-01-17 14:46:27 -0600

    Updated CV section, removed skills section

--- pages/resume.js
+++ pages/resume.js
@@ -42,14 +42,14 @@ const index = () => {
               <div className="container bg-color-810 dark:bg-[#0D0D0D] py-12 px-4 sm:px-5 md:px-10 lg:px-20">
                 <div className="grid grid-cols-1  md:grid-cols-2 gap-8">
                   <div className="col-span-1">
-                    <h4 className="text-5xl dark:text-white font-medium mb-6">
+                    {/* <h4 className="text-5xl dark:text-white font-medium mb-6">
                       Working Skills
                     </h4>
-                    <LineItem />
+                    <LineItem /> */}
                     {/* experience percent items */}
                   </div>
-                  <div className="col-span-1">
-                    <h4 className="text-5xl dark:text-white font-medium mb-8">
+                  <div className="col-span-12">
+                    <h4 className="text-5xl text-center dark:text-white font-medium mb-8">
                       Knowledges
                     </h4>
 
