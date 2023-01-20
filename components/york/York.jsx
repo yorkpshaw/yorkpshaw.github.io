6340adf5793d025fc74ca62c14fe91e35c6b9970
York Shaw <york.p.shaw@gmail.com>
2023-01-20 13:08:12 -0600

    Revised about me

--- components/york/York.jsx
+++ components/york/York.jsx
@@ -12,15 +12,15 @@ const York = () => {
         </p>
         I was crazy enough to want to do it professionally. Spending hours in a practice room and diving deep into the music was something that never felt like work. After all, we're supposed to try to find work that doesn't feel like work, right?
         <p>
-        I had spent thousands of hours practicing, rehearsing, and performing for the next 15 years or so. I had fulfilled a lot of dreams, but I reached a point where I felt ready to do something different, but I wanted to do something that could also connect with people and make an impact.
-        A friend introduced coding to me. I sort of had an idea what it was, and had heard the words "JavaScript" and "Python" before, but I didn't know what they meant. Being the curious cat that I am, I investigated.
+        I had spent thousands of hours practicing, rehearsing, and performing for the next 15 years or so. I had fulfilled a lot of dreams, but I reached a point where I felt ready to do something different. I wanted to do something that could also connect with people and make an impact, but I didn't know what that could be...until one day, when a friend introduced coding to me.
+        I sort of had an idea what it was, and I had heard the words "JavaScript" and "Python" before, but I didn't know what they meant. Being the curious cat that I am, I investigated.
         </p>
         <p>
         The more investigating I did, the more I kept going down the rabbit hole of web development. I spent hours and hours on YouTube and Udemy, learning all that I could, being fascinated by the world of technology. Once I realized that I was doing this for fun, I knew I had found the right thing in software engineering.
         I enrolled in a full-stack software engineering bootcamp with Hack Reactor to further develop my skills, and the rest is history.
         </p>
         <p>
-        When I'm not coding or playing the violin, you might find me doing one of the following:
+        Thanks for reading about me! When I'm not coding or playing the violin, you might find me doing one of the following:
         </p>
         <ul className="list-disc">
         <li>Goofing off with my dog, Onyx</li>
