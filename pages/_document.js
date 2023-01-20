a4c97766339210545e2ce79afbbe2436933ec506
York Shaw <york.p.shaw@gmail.com>
2023-01-19 18:20:34 -0600

    Got rid of random characters in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-np        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
         <meta name="theme-color" content="#ffffff"></meta>
 
       </Head>
